f953d44b79a399022db946cf1a41a7004b8be508
Anty12 <59312736+Anty12@users.noreply.github.com>
2025-05-24 17:59:51 +0200

    Update code for Rozdzial1

--- ButtonHitbox.cs
+++ ButtonHitbox.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 public class ButtonHitbox : MonoBehaviour
 {
+	public TMP_Text text_tmp;
+	
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+       this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
     }
 
-
+	public void ViewText(string text)
+    {
+        text_tmp.text = "Ups...\nTo nie jest Francja, a " + text + ".\nNastępnym razem ci się uda!!!";
+    }
 }
